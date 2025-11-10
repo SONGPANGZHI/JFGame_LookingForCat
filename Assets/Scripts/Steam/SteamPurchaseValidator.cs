d31319f00480daaefb67aa013d94f45815aff4d4
1608804518@qq.com <1608804518@qq.com>
2025-11-10 10:39:44 +0800

    移除Steam sdk

--- Assets/Scripts/Steam/SteamPurchaseValidator.cs
+++ Assets/Scripts/Steam/SteamPurchaseValidator.cs
@@ -1,44 +1,44 @@
-﻿using Steamworks;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using Steamworks;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class SteamPurchaseValidator : MonoBehaviour
-{
-    [Header("是否在开发阶段跳过购买验证")]
-    public bool skipCheckInEditor = true;
+//public class SteamPurchaseValidator : MonoBehaviour
+//{
+//    [Header("是否在开发阶段跳过购买验证")]
+//    public bool skipCheckInEditor = true;
 
-    void Start()
-    {
-#if UNITY_EDITOR
-        if (skipCheckInEditor)
-        {
-            Debug.Log("开发模式：跳过购买验证（编辑器中）");
-            return;
-        }
-#endif
+//    void Start()
+//    {
+//#if UNITY_EDITOR
+//        if (skipCheckInEditor)
+//        {
+//            Debug.Log("开发模式：跳过购买验证（编辑器中）");
+//            return;
+//        }
+//#endif
 
-        if (!SteamAPI.Init())
-        {
-            Debug.LogWarning("Steam 未初始化，跳过验证。");
-            return;
-        }
+//        if (!SteamAPI.Init())
+//        {
+//            Debug.LogWarning("Steam 未初始化，跳过验证。");
+//            return;
+//        }
 
-        AppId_t appId = (AppId_t)SteamUtils.GetAppID();
-        bool hasLicense = SteamApps.BIsSubscribedApp(appId);
+//        AppId_t appId = (AppId_t)SteamUtils.GetAppID();
+//        bool hasLicense = SteamApps.BIsSubscribedApp(appId);
 
-        if (hasLicense)
-        {
-            Debug.LogWarning("当前用户未购买游戏！(AppID: " + appId + ")");
-            Debug.Log("当前用户已购买此游戏。");
-        }
-        else
-        {
-            Debug.LogWarning("当前用户未购买游戏！(AppID: " + appId + ")");
-#if !UNITY_EDITOR
-            // 可在正式版中退出或提示购买
-            // Application.Quit();
-#endif
-        }
-    }
-}
+//        if (hasLicense)
+//        {
+//            Debug.LogWarning("当前用户未购买游戏！(AppID: " + appId + ")");
+//            Debug.Log("当前用户已购买此游戏。");
+//        }
+//        else
+//        {
+//            Debug.LogWarning("当前用户未购买游戏！(AppID: " + appId + ")");
+//#if !UNITY_EDITOR
+//            // 可在正式版中退出或提示购买
+//            // Application.Quit();
+//#endif
+//        }
+//    }
+//}
